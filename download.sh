8f7cae81fbb71fe0b9ce3741e7686f36cb3ded5a
Pooya Kabiri <pooya_kabiri@yahoo.com>
2021-07-16 17:10:05 +0430

    Modify for cobra

--- download.sh
+++ download.sh
@@ -5,7 +5,7 @@ cd temps
 mkdir smpl; cd smpl
 
 gdown --id '1Pdcn7wwFbu3sLbhTtEPS2dp7qQVAw12-'
-gdown --id '14d1IgUPOVLGKqQ3kj8Iva8MfcvIBIuvY'
+# gdown --id '14d1IgUPOVLGKqQ3kj8Iva8MfcvIBIuvY'
 
 mkdir -p ../../smpl/models/smpl
 cd ../../smpl/models/smpl
@@ -13,6 +13,8 @@ cd ../../smpl/models/smpl
 rsync -r ../../../temps/smpl/ .
 
 unzip "*.zip"
+mv smpl/* .
+rm -rf smpl
 cd ../../..
 
 # Data Files
@@ -27,7 +29,10 @@ cd ../../data
 
 rsync -r ../temps/data/ .
 unzip "*.zip"
+mv data/* .
 
+rm -rf data/
+rm -rf *.zip
 cd ../
 
 rm -rf temps/
