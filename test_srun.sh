3b6c9468643973ae25feae488e088cd19ccd7142
Pooya Kabiri <pooya_kabiri@yahoo.com>
2021-07-16 18:38:07 +0430

    Modify for cobra

---
+++ test_srun.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -l
+#
+## This is your working directory
+#SRUN -D /u/gtiwari/SCANimate/
+#
+# Job Name Shown in the Queue (squeue)
+#SRUN -J SCANimate
+#
+# We are interested in nodes with GPUs, i.e., GPU Queue (Partition):
+#SRUN --partition=gpu
+#
+# Node feature:
+#SRUN --constraint="gpu"
+#SRUN --gres=gpu:rtx5000:2       # If using only 1 GPU of a shared node
+#SRUN --mem=92500
+#
+# Number of nodes and MPI tasks per node:
+#SRUN --nodes=1
+#SRUN --ntasks-per-node=1     # This stays 1 for 2 GPU training too, in most cases
+#SRUN --cpus-per-task=16      # Number of processes ?? (Each GPU has 16 workers ??)
+#
+#
+# wall clock limit:
+#SRUN --time=1:00:00         #Specify a smaller duration if your script takes less time
+
+module load gcc/8
+module load cuda/10.1
+module load anaconda/3/2020.02
+
+source /mpcdf/soft/SLE_12/packages/x86_64/anaconda/3/2020.02/etc/profile.d/conda.sh
+conda activate scanimate
+
+python -m ./apps.test_scanimate -c ./data/pretrained_configs/release_03223_shortlong.yaml -t ./data/test/gLO_sBM_cAll_d14_mLO1_ch05
+python ./render/render_aist.py -i demo_result/results_test/release_03223_shortlong_test_gLO_sBM_cAll_d14_mLO1_ch05/ -o demo_result
